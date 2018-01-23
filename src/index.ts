54b5fc0401aae5c0de286fd3583d23e9cc2d92a9
Márvio André Bezerra Silverio <marvio.bezerra@hotmail.com>
2018-01-22 20:17:47 -0500

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import * as THREE from "three";
+
+export class Index {
+    constructor() {
+        this.Init();
+    }
+
+    public Init(): void {
+        var scene = new THREE.Scene();
+        var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        var fac = 0.1;
+
+        var renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(renderer.domElement);
+
+        var geometry = new THREE.BoxGeometry(1, 1, 1);
+        var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        var cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+
+        camera.position.z = 5;
+
+        var animate = function () {
+            requestAnimationFrame(animate);
+
+            cube.rotation.x += 0.1;
+            cube.rotation.y += 0.1;
+
+            camera.position.z += fac;
+
+            if (camera.position.z > 10 || camera.position.z < -10) {
+                fac = fac * -1;
+            }
+
+            console.log(camera.position.x);
+
+            renderer.render(scene, camera);
+        };
+
+        animate();
+    }
+}
+
+const index = new Index();
+index.Init();
